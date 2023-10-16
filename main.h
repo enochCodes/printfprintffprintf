c27ee6f6946bff603e760071872e37d1577c64e4
enochCodes <henoksamson57@gmail.com>
2023-10-16 14:17:37 +0000

    remove the c make r

--- main.h
+++ main.h
@@ -1,11 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <limits.h>
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+
  
 
 int _printf(const char *format, ...);
